ace72c900c965b86b02ca3e5d282f4d5c4c29e8a
simon lee <simonleelcf@gmail.com>
2020-06-02 20:26:00 +1000

    adding npm packages

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const app = express();
-const routes = require("./routes");
+const routes = require('./routes');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const logger = require('./utils/logger');
 const PORT = process.env.PORT || 3000;
 
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+} else {
+    app.use(morgan('common'));
+}
+
+app.use(helmet());
 app.use(routes);
 
-app.listen(PORT, () => console.log("listen at port 3000"));
+app.listen(PORT, () => logger.info('listen at port 3000'));
